783dc4bbb3380bb2a7142de125c8b4449357186a
Edie Lemoine <edie@myparcel.nl>
2022-04-19 16:57:43 +0200

    update styling

--- src/.vuepress/theme/client/services/tailwind/variants/iconVariant.ts
+++ src/.vuepress/theme/client/services/tailwind/variants/iconVariant.ts
@@ -3,8 +3,9 @@ import { defineVariant } from '../defineVariant';
 export const iconVariant = defineVariant('icon',
   {
     'api-reference': 'icon-api-reference',
-    'chevron-left': ['icon-chevron-right', 'transform', 'rotate-180'],
-    'chevron-down': ['icon-chevron-right', 'transform', 'rotate-240'],
+    'chevron-up': 'icon-chevron-up',
+    'chevron-left': 'icon-chevron-left',
+    'chevron-down': 'icon-chevron-down',
     'chevron-right': 'icon-chevron-right',
     documentation: 'icon-documentation',
     integrations: 'icon-integrations',
