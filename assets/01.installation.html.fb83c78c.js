ff755be98435cc3aa3d3ebd896f30018587af149
EdieLemoine <edie.lemoine@gmail.com>
2022-05-30 11:42:46 +0000

    Deploying to gh-pages from @ myparcelnl/developer@df16d3f6ee682dd0622de91fa0b5ede10277562d 🚀

--- assets/01.installation.html.fb83c78c.js
+++
@@ -1,6 +0,0 @@
-import{K as a}from"./app.485a03b2.js";import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";const s={},t=a(`<h1 id="installation" tabindex="-1"><a class="header-anchor" href="#installation" aria-hidden="true">#</a> Installation</h1><div class="language-bash ext-sh"><pre class="language-bash"><code> <span class="token comment"># Using Yarn</span>
-<span class="token function">yarn</span> <span class="token function">add</span> @myparcel/sdk
-
- <span class="token comment"># Or NPM</span>
-<span class="token function">npm</span> i @myparcel/sdk
-</code></pre></div>`,2);function e(c,o){return t}var r=n(s,[["render",e],["__file","01.installation.html.vue"]]);export{r as default};
---
+++ assets/index.html.edecba9f.js
@@ -0,0 +1,1 @@
+const e={key:"v-0c0aa0a2",path:"/documentation/50.php-sdk/",title:"Introduction",lang:"en-GB",frontmatter:{title:"Introduction"},excerpt:"",headers:[],git:{updatedTime:165390416e4,contributors:[{name:"Edie Lemoine",email:"edie@myparcel.nl",commits:1}]},filePathRelative:"documentation/50.php-sdk/readme.md"};export{e as data};
