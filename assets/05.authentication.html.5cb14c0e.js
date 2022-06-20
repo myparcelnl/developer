e6fefffd48f220891bac62a971341467e051fe22
EdieLemoine <edie.lemoine@gmail.com>
2022-06-20 11:38:22 +0000

    Deploying to gh-pages from @ myparcelnl/developer@f5deb0d0c272780d7299ce1ec976a4b969057999 🚀

--- assets/05.authentication.html.5cb14c0e.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-185271f0",path:"/api-reference/05.authentication.html",title:"5. Authentication",lang:"en-GB",frontmatter:{},excerpt:"",headers:[{level:2,title:"5.1 Error codes",slug:"_5-1",children:[]}],git:{updatedTime:1655717163e3,contributors:[{name:"Edie Lemoine",email:"edie@myparcel.nl",commits:1}]},filePathRelative:"api-reference/05.authentication.md"};export{e as data};
---
+++ assets/index.html.a407b24c.js
@@ -0,0 +1,1 @@
+const t={key:"v-56f52154",path:"/documentation/13.magento2/",title:"Introduction",lang:"en-GB",frontmatter:{title:"Introduction"},excerpt:"",headers:[],git:{updatedTime:1655724977e3,contributors:[{name:"DevFrenk",email:"40214007+DevFrenk@users.noreply.github.com",commits:1}]},filePathRelative:"documentation/13.magento2/readme.md"};export{t as data};
