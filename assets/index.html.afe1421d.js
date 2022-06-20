af59028a0bc78b5115bca4662f36b8d0f9608daa
EdieLemoine <edie.lemoine@gmail.com>
2022-06-20 08:55:09 +0000

    Deploying to gh-pages from @ myparcelnl/developer@48b242b3a35328e12c7282ea7cda64c3b8450fdf 🚀

---
+++ assets/index.html.afe1421d.js
@@ -0,0 +1,1 @@
+const e={key:"v-56f52154",path:"/documentation/13.magento2/",title:"Introduction",lang:"en-GB",frontmatter:{title:"Introduction"},excerpt:"",headers:[],git:{updatedTime:1655715154e3,contributors:[{name:"Edie Lemoine",email:"edie@myparcel.nl",commits:1}]},filePathRelative:"documentation/13.magento2/readme.md"};export{e as data};
