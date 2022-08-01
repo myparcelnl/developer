8e68199273d4ea211997dc0978cd88f9dfb6f31b
EdieLemoine <edie.lemoine@gmail.com>
2022-08-01 08:29:54 +0000

    Deploying to gh-pages from @ myparcelnl/developer@c2dbed9faee4374ab9722e062f075c92e2da52b1 🚀

---
+++ assets/NpmShield.3dc16db9.js
@@ -0,0 +1,1 @@
+import m from"./Shield.ced1b5ef.js";import{a6 as o}from"./app.bcb55c99.js";import"./AutoLink.63af6e2f.js";import"./plugin-vue_export-helper.21dcd24c.js";import"./MPImg.a5d1a4db.js";const c=t=>{var a;return o(m,{alt:t.alt,href:`https://npmjs.com/package/${t.package}`,path:`npm/${(a=t.type)!=null?a:"v"}/${t.package}`,color:"CC3534",logo:"npm"})};export{c as default};
--- assets/useTranslate.8bdc925c.js
+++
@@ -1,1 +0,0 @@
-import{p as r}from"./app.d477afc0.js";const o=()=>{const a=r();return e=>a.value.hasOwnProperty(e)?a.value[e]:""};export{o as u};
