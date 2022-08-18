53cfc1150f407df45a9de202885ec0894d2a7ff6
Edie Lemoine <edie@myparcel.nl>
2022-08-18 12:48:58 +0200

    fix(sitemap): remove index.html from urls

--- src/.vuepress/plugins/sitemap.ts
+++ src/.vuepress/plugins/sitemap.ts
@@ -17,7 +17,6 @@ export const sitemapPlugin = (config: SitemapPluginConfig): Plugin => ({
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
     app.pages
-      .filter((page) => page.htmlFilePathRelative.endsWith('.html'))
       .forEach((page) => {
         const pageDate = page.date === '0000-00-00' ? undefined : page.date;
         let timestamp: string | number | undefined = pageDate;
@@ -26,8 +25,14 @@ export const sitemapPlugin = (config: SitemapPluginConfig): Plugin => ({
           timestamp = page.data.git.updatedTime ?? pageDate;
         }
 
+        const htmlPath = page.htmlFilePathRelative.replace(/\/index\.html$/, '/');
+
+        if (htmlPath === 'index.html') {
+          return;
+        }
+
         sitemap += '\n  <url>';
-        sitemap += `\n    <loc>${config.baseUrl}${page.htmlFilePathRelative}</loc>`;
+        sitemap += `\n    <loc>${config.baseUrl}${htmlPath}</loc>`;
         if (timestamp) {
           const date = dayjs(timestamp).format('YYYY-MM-DDTHH:mm:ssZ');
           sitemap += `\n    <lastmod>${date}</lastmod>`;
