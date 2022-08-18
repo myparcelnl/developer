12fd3e1a750338db15e1232d9c7ebe0e93b84af7
EdieLemoine <edie.lemoine@gmail.com>
2022-08-18 10:50:35 +0000

    Deploying to gh-pages from @ myparcelnl/developer@53cfc1150f407df45a9de202885ec0894d2a7ff6 🚀

---
+++ assets/NpmShield.aba1b7b3.js
@@ -0,0 +1,1 @@
+import m from"./Shield.308ef7e2.js";import{a7 as o}from"./app.bb55278a.js";import"./AutoLink.8eebd4be.js";import"./plugin-vue_export-helper.21dcd24c.js";import"./MPImg.68aa24a8.js";const c=t=>{var a;return o(m,{alt:t.alt,href:`https://npmjs.com/package/${t.package}`,path:`npm/${(a=t.type)!=null?a:"v"}/${t.package}`,color:"CC3534",logo:"npm"})};export{c as default};
