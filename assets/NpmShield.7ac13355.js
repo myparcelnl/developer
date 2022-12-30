7af3905b584d59ec45dc8d1f84db159886d8fa6c
RichardPerdaan <richard@myparcel.nl>
2022-12-30 08:14:35 +0000

    Deploying to gh-pages from @ myparcelnl/developer@7436141cf259c602af4a0d64536e3d940f6fd2bc 🚀

--- assets/NpmShield.7ac13355.js
+++
@@ -1,1 +0,0 @@
-import m from"./Shield.e701c82b.js";import{a7 as o}from"./app.09abaed2.js";import"./AutoLink.c2b3e827.js";import"./plugin-vue_export-helper.21dcd24c.js";import"./MPImg.ffce6e8d.js";const c=t=>{var a;return o(m,{alt:t.alt,href:`https://npmjs.com/package/${t.package}`,path:`npm/${(a=t.type)!=null?a:"v"}/${t.package}`,color:"CC3534",logo:"npm"})};export{c as default};
---
+++ assets/useTranslate.e05f153a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app.bbdcc39b.js";const o=()=>{const a=r();return e=>a.value.hasOwnProperty(e)?a.value[e]:""};export{o as u};
