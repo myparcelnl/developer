4a65bb229e5faeb568119645a0f7f57d1119781c
Edie Lemoine <edie@myparcel.nl>
2023-03-10 09:45:25 +0100

    fix: fix sidebar behaving weirdly (#48)

--- src/.vuepress/theme/client/views/layout/header/toggle-sidebar-button/ToggleSidebarButton.vue
+++ src/.vuepress/theme/client/views/layout/header/toggle-sidebar-button/ToggleSidebarButton.vue
@@ -1,74 +1,46 @@
 <template>
   <div
-    v-if="hasSidebar"
+    v-if="exists"
     class="cursor-pointer flex lg:hidden p-3"
     :title="translate('toggleSidebar')"
     :aria-label="translate('toggleSidebar')"
     aria-expanded="false"
     role="button"
     tabindex="0"
-    @click="toggle">
+    @click="() => toggle()">
     <div
+      role="none"
       class="flex flex-col h-6 items-center justify-around w-6"
       aria-hidden="true"
       tabindex="-1">
       <span
-        :class="[
-          burgerClasses,
-          {
-            '-rotate-45 translate-y-2': toggled,
-          },
-        ]" />
+        role="none"
+        :class="[burgerClasses, { '-rotate-45 translate-y-2': toggled }]" />
       <span
-        :class="[
-          burgerClasses,
-          {
-            'scale-x-0': toggled,
-          },
-        ]" />
+        role="none"
+        :class="[burgerClasses, { 'scale-x-0': toggled }]" />
       <span
-        :class="[
-          burgerClasses,
-          {
-            'rotate-45 -translate-y-2': toggled,
-          },
-        ]" />
+        role="none"
+        :class="[burgerClasses, { 'rotate-45 -translate-y-2': toggled }]" />
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { useSidebar, useSiteLocaleData, useTranslate } from '@mptheme/client/composables';
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { useSidebar, useTranslate } from '../../../../composables';
 
-export default defineComponent({
-  name: 'ToggleSidebarButton',
-  emits: ['toggle'],
-  setup: (props, ctx) => {
-    const sidebar = useSidebar();
-    const translate = useTranslate();
+const { toggled, toggle, exists } = useSidebar();
 
-    return {
-      translate,
-      toggled: sidebar.isOpen,
-      hasSidebar: sidebar.exists,
+const translate = useTranslate();
 
-      toggle() {
-        sidebar.toggle();
-        ctx.emit('toggle');
-      },
-
-      burgerClasses: [
-        'bg-black',
-        'dark:bg-white',
-        'duration-100',
-        'h-0.5',
-        'inline-block',
-        'rounded-lg',
-        'transition-transform',
-        'w-full',
-      ],
-    };
-  },
-});
+const burgerClasses = [
+  'bg-black',
+  'dark:bg-white',
+  'duration-100',
+  'h-0.5',
+  'inline-block',
+  'rounded-lg',
+  'transition-transform',
+  'w-full',
+];
 </script>
