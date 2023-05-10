40c5e8ded2cd6c677d60b07f5dd58084d0f80419
Edie Lemoine <edie@myparcel.nl>
2023-05-10 13:08:10 +0200

    chore: update eslint and prettier configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,92 +1,53 @@
-const path = require('path');
-
 module.exports = {
   root: true,
-  extends: '@myparcel/eslint-config/preset-typescript',
-  parserOptions:{
-    project: path.resolve(__dirname, 'tsconfig.json'),
-  },
-  rules: {
-    '@typescript-eslint/require-await': 'off',
-    'class-methods-use-this': 'off',
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: '*', next: 'if' },
-      { blankLine: 'always', prev: 'if', next: '*' },
-      { blankLine: 'never', prev: 'const', next: 'const' },
-      { blankLine: 'always', prev: 'const', next: 'multiline-const' },
-      { blankLine: 'always', prev: 'multiline-const', next: 'multiline-const' },
-    ],
-    'space-before-blocks': ['error', 'always'],
-  },
   overrides: [
-    // Vue and related files
     {
-      extends: [
-        '@myparcel/eslint-config/preset-vue3-typescript',
-        'plugin:vue/vue3-recommended',
-        '@vue/eslint-config-typescript',
-      ],
-      files: [
-        '!./src/.vuepress/**/*.spec.*',
-        '!./src/.vuepress/*.js',
-        '!./src/.vuepress/*.ts',
-        './src/.vuepress/**/*',
-      ],
+      files: ['./**/*.vue'],
+      extends: '@myparcel-eslint/eslint-config-prettier-typescript-vue',
       rules: {
-        'vue/multi-word-component-names': 'off',
-        'vue/no-unregistered-components': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+        'vue/html-self-closing': 'off',
+        'vue/no-bare-strings-in-template': 'off',
         'vue/no-undef-components': [
           'error',
           {
-            ignorePatterns: [
-              'AutoLink',
-              'Badge',
-              'CodeGroup',
-              'CodeGroupItem',
-              'Content',
-              'ExternalLinkIcon',
-              'HomeLink',
-              'NavbarSearch',
-              'RouterLink',
-              'RouterView',
-              'Transition',
-              'TransitionGroup',
-            ],
+            ignorePatterns: ['^Pdk(?:\\w)+$'],
           },
         ],
-        'vue/no-bare-strings-in-template': 'off',
-
-        '@typescript-eslint/restrict-plus-operands': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-
-        // These rules are extremely slow
-        '@typescript-eslint/no-misused-promises': 'off',
-        '@typescript-eslint/promise-function-async': 'off',
       },
     },
-
     {
-      // Config files and test specs
-      files: [
-        './*.cjs',
-        './*.mjs',
-        './*.ts',
-        './src/.vuepress/**/*.spec.*',
-        './src/.vuepress/*.mjs',
-        './src/.vuepress/*.ts',
-      ],
-      env: {
-        node: true,
-      },
+      files: ['./**/*.ts', './**/*.tsx'],
+      extends: '@myparcel-eslint/eslint-config-prettier-typescript',
       rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+        'class-methods-use-this': 'off',
+      },
+    },
+    {
+      files: ['./**/*.js', './**/*.mjs'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+    {
+      files: ['./**/*.js', './**/*.cjs', './**/*.mjs'],
+      extends: [
+        '@myparcel-eslint/eslint-config-node',
+        '@myparcel-eslint/eslint-config-esnext',
+        '@myparcel-eslint/eslint-config-prettier',
+      ],
+    },
+    {
+      files: ['./**/*.spec.*', './**/*.test.*', './**/__tests__/**', './**/*Test.*'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-magic-numbers': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
         'max-len': 'off',
         'max-lines-per-function': 'off',
-        'no-magic-numbers': 'off',
-        'vue/max-len': 'off',
       },
     },
   ],
 };
+
