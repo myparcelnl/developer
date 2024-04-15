6b1e0be56d3be61c9d3f9189cefd81288b96bd72
Freek van Rijt <freek.vanrijt@myparcel.nl>
2024-04-15 16:36:13 +0200

    feat(open-api): support display of webhooks and make paths optional
    INT-407

--- src/.vuepress/plugins/openApi.ts
+++ src/.vuepress/plugins/openApi.ts
@@ -14,51 +14,56 @@ export const openApiPlugin = (config: OpenApiPluginConfig): Plugin => ({
     // Find all yaml files in config.yamlUrls and create a page for each of them
     await Promise.all(
       config.yamlUrls?.map(async (url) => {
-        const spec = (await fetch(url).then(async (res) => parseYaml(await res.text()))) as OpenApiType.Document;
+        const document = (await fetch(url).then(async (res) => parseYaml(await res.text()))) as OpenApiType.Document;
         // Use the basename of the file as the slug
-        const slug = kebabCase(spec.info.title);
+        const resolvedDocument = resolveRefs(document);
 
         // Now generate the page contents
-        app.pages.push(
-          await createPage(app, {
-            path: `/api-reference/${slug}`,
-            content: `\
----
-title: ${spec.info.title}
-description: ${spec.info.description}
----
-Version ${spec.info.version}
-
-${spec.info.description ?? ''}
-
-${spec.security ? '## Authorization' : ''}
-<OpenApiSecurityRequirements
-:security='${JSON.stringify(spec.security ?? [])}'
-:security-schemes='${JSON.stringify(spec.components?.securitySchemes ?? [])}' />
-
-## Endpoints
-${renderPaths(spec)}
-                  `,
-          }),
-        );
+        app.pages.push(await createPage(app, generateOpenApiPage(resolvedDocument)));
       }),
     );
   },
 });
 
-function renderPaths(document: OpenApiType.Document): string {
-  const resolvedDocument = resolveRefs(document);
+function generateOpenApiPage(document: OpenApiType.Document) {
+  return {
+    path: `/api-reference/${kebabCase(document.info.title)}`,
+    content: `\
+---
+title: ${document.info.title}
+description: ${document.info.description}
+---
+Version ${document.info.version}
 
-  let chapters = '';
+${document.info.description ?? ''}
 
-  if (resolvedDocument.paths) {
-    for (const [path, pathObj] of Object.entries(resolvedDocument.paths)) {
-      chapters += `
+${document.security ? '## Authorization' : ''}
+<OpenApiSecurityRequirements
+:security='${JSON.stringify(document.security ?? [])}'
+:security-schemes='${JSON.stringify(document.components?.securitySchemes ?? [])}' />
+
+${renderPaths(document, document.paths, 'Endpoints')}
+
+${renderPaths(document, document.webhooks, 'Webhooks')}
+                  `,
+  };
+}
+
+function renderPaths(
+  document: OpenApiType.Document,
+  paths: OpenApiType.PathsObject | undefined,
+  heading: string,
+): string {
+  if (!paths || !Object.keys(paths).length) return '';
+
+  let chapters = `## ${heading}`;
+
+  for (const [path, pathObj] of Object.entries(paths)) {
+    chapters += `
 ### ${path}
 \n
 <OpenApiPath :path='${JSON.stringify(pathObj)}' :components='${JSON.stringify(document.components)}' title='${path}' />
     `;
-    }
   }
 
   return chapters;
